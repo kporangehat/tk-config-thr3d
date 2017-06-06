7dc76331f8aa82decaf652642aa356163a20a4d6
ali.jafargholi <ali.jafargholi@gmail.com>
2017-06-06 14:28:30 -0400

    Implemented the Save Iteration. Added the function to switch the task status to In Progress when a DCC is launched.

--- hooks/before_app_launch.py
+++ hooks/before_app_launch.py
@@ -21,6 +21,13 @@ import logging
 import tank
 
 
+# TODO: Add this to path via systems(This is too hard coded)
+sys.path.append(r"\\isln-smb\thr3dcgi_config\Apps\Python27\Lib\site-packages")
+sys.path.append(r"\\isln-smb\thr3dcgi_config\Agnostic")
+
+from shotgun_utils import project_util
+
+
 class BeforeAppLaunch(tank.Hook):
     """
     Hook to set up the system prior to app launch.
@@ -42,21 +49,48 @@ class BeforeAppLaunch(tank.Hook):
         # accessing the current context (current shot, etc)
         # can be done via the parent object
         multi_launchapp = self.parent
-        current_entity = multi_launchapp.context.entity
 
         logging.info("Setting Global Variable for THR3d")
 
+        entity = multi_launchapp.context.task
+        if not entity:
+            entity = multi_launchapp.context.entity
+
+        if entity:
+
+            type_ = entity.get('type')
+            id_ = entity.get('id')
+            filters = [['id', 'is', id_]]
+
+            # find the current task info
+            task = project_util.get_entity(filters=filters,
+                                           entity_type=type_,
+                                           find_="one")
+
+            # Change the task's status
+            result = project_util.set_status(task, 'ip')
+            if result:
+                logging.info("Updated the status of entity: {} - id: {} to "
+                             "{}".format(result.get('name'),
+                                         result.get('id'),
+                                         result.get('sg_status_list')))
+            else:
+                logging.info("Failed to update the status.")
+
         # Engine specific functions
-        multi_launchapp = self.parent
+        if multi_launchapp.get_setting("engine") == "tk-nuke":
+            logging.info("Running Before Launch Functions for Nuke")
+            logging.info("app_path == > ", app_path)
+
         if multi_launchapp.get_setting("engine") == "tk-houdini":
-            logging.info("Running Houdini")
-            logging.info("Running Houdini")
-            os.environ["HOUDINI_USE_OTL_AS_DEFAULT_HDA_EXT"] = 1
+            logging.info("Running Before Launch Functions for Houdini")
+            os.environ["HOUDINI_USE_OTL_AS_DEFAULT_HDA_EXT"] = "1"
             os.environ["HOUDINI_OTLSCAN_PATH"] = r"\\isln-smb\thr3dcgi_config\Houdini\otl"
 
         if multi_launchapp.get_setting("engine") == "tk-maya":
+            logging.info("Running Before Launch Functions for Maya")
             logging.info("Running Maya", version)
             logging.info("app_path == > ", app_path)
             logging.info("app_args == > ", app_args)
             logging.info("kwargs == > ", kwargs)
-            logging.info("current_entity == > ", current_entity)
+            logging.info("current_entity == > ", entity)
