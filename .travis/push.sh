87a2b4ded400c5c272e760f35814c27fa261e127
KP <kp@shotgunsoftware.com>
2017-08-10 12:58:53 -0400

    new config stuff and trying to push back to github

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,24 +1,49 @@
-#!/bin/sh
+#!/bin/bash
 
-setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
-}
+set -e # Exit with nonzero exit code if anything fails
 
-commit_template_files() {
-  git checkout -b ${TRAVIS_BRANCH}
-  git add core/templates/templates.*.yml
-  git commit --message "[Travis build ${TRAVIS_BUILD_NUMBER}] Auto sync templates files for all facilities [ci-skip]"
-}
+# SOURCE_BRANCH="master"
+# TARGET_BRANCH="gh-pages"
 
-upload_files() {
-  git remote -v
-  git push origin $TRAVIS_BRANCH
-  git remote add origin-push https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
-  git remote -v
-  git push --quiet --set-upstream origin-push ${TRAVIS_BRANCH}
-}
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    echo "Skipping pull request."
+    exit 0
+fi
 
-setup_git
-commit_template_files
-upload_files
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+
+# Setup git
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+# check out our branch
+git checkout ${TRAVIS_BRANCH}
+
+# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+if git diff --quiet; then
+    echo "No changes to the output on this push; exiting."
+    exit 0
+fi
+
+# Add our new template files
+git add core/templates/templates.*.yml
+
+# Commit them locally
+git commit --message "[Travis build ${TRAVIS_BUILD_NUMBER}] Auto sync templates files for all facilities [ci-skip]"
+
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in id_rsa_thr3d_deploy.pub.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+
+
