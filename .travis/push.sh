b3b702e525c536ab7f1301acdbf830c34debf83b
KP <kp@shotgunsoftware.com>
2017-08-10 15:51:47 -0400

    debug vars for openssl

--- .travis/push.sh
+++ .travis/push.sh
@@ -40,6 +40,9 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+echo "running openssl"
+echo "ENCRYPTED_KEY: $ENCRYPTED_KEY"
+echo "ENCRYPTED_IV: $ENCRYPTED_IV"
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in id_rsa_thr3d_deploy.pub.enc -out deploy_key -d
 chmod 600 deploy_key
 eval `ssh-agent -s`
